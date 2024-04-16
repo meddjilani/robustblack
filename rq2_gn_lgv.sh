37b437199c77e95a9656696d0d85bdecdec8ca04
meddjilani <djilanimedben@gmail.com>
2024-04-16 16:45:33 +0200

    rq2 job ghost and lgv

---
+++ rq2_gn_lgv.sh
@@ -0,0 +1,17 @@
+my_list=( "Liu2023Comprehensive_ConvNeXt-L" "Bai2024MixedNUTS" "Liu2023Comprehensive_Swin-L" )
+
+train_path="/mnt/data/data/mdjilani/dataset/Imagenet/Sample_49000"  # Removed leading /
+test_path="/mnt/data/data/mdjilani/dataset/Imagenet/Sample_1000"  # Removed leading /
+
+for seed in 42 1 10; do
+  for target in "${my_list[@]}"; do
+
+    cd LGV
+    python LGV-MI-FGSM.py --seed $seed --target $target --data_path $test_path --train_path $train_path --gpu cuda --model wide_resnet101_2 --batch_size 64 --lgv_models "/mnt/data/data/mdjilani/lgv_models_robust"
+  
+    cd GHOST
+    python LGV-MI-FGSM.py --seed $seed --target $target --data_path $test_path --gpu cuda --model wide_resnet101_2 --batch_size 64
+
+    cd ..
+  done
+done
