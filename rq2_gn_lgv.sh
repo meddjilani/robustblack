39d5a7d9f358e648302dcfc3589579f046abbaf7
meddjilani <djilanimedben@gmail.com>
2024-04-16 16:47:14 +0200

    specify rq2 comet project

--- rq2_gn_lgv.sh
+++ rq2_gn_lgv.sh
@@ -7,10 +7,10 @@ for seed in 42 1 10; do
   for target in "${my_list[@]}"; do
 
     cd LGV
-    python LGV-MI-FGSM.py --seed $seed --target $target --data_path $test_path --train_path $train_path --gpu cuda --model wide_resnet101_2 --batch_size 64 --lgv_models "/mnt/data/data/mdjilani/lgv_models_robust"
-  
+    python LGV-MI-FGSM.py --seed $seed --target $target --data_path $test_path --train_path $train_path --gpu cuda --model wide_resnet101_2 --batch_size 64 --lgv_models "/mnt/data/data/mdjilani/lgv_models_robust" --comet_proj RQ2
+
     cd GHOST
-    python LGV-MI-FGSM.py --seed $seed --target $target --data_path $test_path --gpu cuda --model wide_resnet101_2 --batch_size 64
+    python GN-MI-FGSM.py --seed $seed --target $target --data_path $test_path --gpu cuda --model wide_resnet101_2 --batch_size 64 --comet_proj RQ2
 
     cd ..
   done
