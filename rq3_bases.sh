b7584304c077696af934e6b3a45b85ae2a46e5f3
meddjilani <djilanimedben@gmail.com>
2024-04-18 01:13:15 +0200

    fix num of models n_wb and add save logs and advs

--- rq3_bases.sh
+++ rq3_bases.sh
@@ -2,12 +2,14 @@ my_list=( "Standard_R50" "Ding2020MMA" "Rony2019Decoupling" "Salman2020Do_50_2" 
 
 train_path="/raid/data/mdjilani/dataset/Imagenet/Sample_49000"  # Removed leading /
 test_path="/raid/data/mdjilani/dataset/Imagenet/Sample_1000"  # Removed leading /
+exp_root="/raid/data/mdjilani/bases_exp"  # Removed leading /
+adv_root="/raid/data/mdjilani/bases_adv"  # Removed leading /
 
 for seed in 42 1 10; do
   for target in "${my_list[@]}"; do
 
     cd BASES
-    python query_w_bb.py --iterw 20 --seed $seed --victim $target --data_path $test_path --gpu cuda --models Wong2020Fast Engstrom2019Robustness Debenedetti2022Light_XCiT-M12 --comet_proj RQ3 -robust -untargeted
+    python query_w_bb.py --exp_root $exp_root --adv_root $adv_root --n_wb 3 --iterw 20 --seed $seed --victim $target --data_path $test_path --gpu cuda --models Wong2020Fast Engstrom2019Robustness Debenedetti2022Light_XCiT-M12 --comet_proj RQ3 -robust -untargeted
 
     cd ..
   done
