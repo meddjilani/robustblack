a555031a99bca30d0636fac384bbb3f0c867c12c
meddjilani <djilanimedben@gmail.com>
2024-04-22 14:50:29 +0200

    fix test to data path

--- rq3_tremba.sh
+++ rq3_tremba.sh
@@ -7,7 +7,7 @@ for seed in 42 1 10; do
   for target in "${my_list[@]}"; do
 
     cd TREMBA
-    python attack.py --comet_proj RQ3 --seed $seed --model_name $target --device cuda --config config/attack_untarget.json --train_path $train_path --test_path $test_path
+    python attack.py --comet_proj RQ3 --seed $seed --model_name $target --device cuda --config config/attack_untarget.json --train_path $train_path --data_path $test_path
 
     cd ..
   done
