5a124bf686950f12cf4979e14e309dd9278ce484
meddjilani <djilanimedben@gmail.com>
2024-04-22 14:43:13 +0200

    fix going back to parent dir

--- rq3_tremba.sh
+++ rq3_tremba.sh
@@ -6,7 +6,7 @@ test_path="/raid/data/mdjilani/dataset/Imagenet/Sample_1000"  # Removed leading 
 for seed in 42 1 10; do
   for target in "${my_list[@]}"; do
 
-    cd ../TREMBA
+    cd TREMBA
     python attack.py --comet_proj RQ3 --seed $seed --model_name $target --device cuda --config config/attack_untarget.json
 
     cd ..
