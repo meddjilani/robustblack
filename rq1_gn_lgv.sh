c4e77184d906670395e632e236ceab6dba3da32a
meddjilani <djilanimedben@gmail.com>
2024-04-30 00:57:37 +0200

    update rq1 rq3 gn and lgv jobs

--- rq1_gn_lgv.sh
+++ rq1_gn_lgv.sh
@@ -1,17 +1,17 @@
 my_list=( "Standard_R50" "Salman2020Do_R18" "Salman2020Do_R50" "Salman2020Do_50_2" "Singh2023Revisiting_ViT-S-ConvStem" "Liu2023Comprehensive_ConvNeXt-B" "Liu2023Comprehensive_Swin-B" "Liu2023Comprehensive_ConvNeXt-L" "Bai2024MixedNUTS" "Liu2023Comprehensive_Swin-L" )
 
-train_path="/raid/data/mdjilani/dataset/Imagenet/Sample_49000"  # Removed leading /
-test_path="/raid/data/mdjilani/dataset/Imagenet/Sample_1000"  # Removed leading /
+test_path="/raid/data/mdjilani/dataset/val"
+helpers_path="/home/mdjilani/robustblack/utils_robustblack"
 
 
 for seed in 42 1 10; do
   for target in "${my_list[@]}"; do
 
     cd GHOST
-    python GN-MI-FGSM.py --comet_proj RQ1 --seed $seed --target $target --data_path $test_path --gpu cuda --model wide_resnet101_2 --batch_size 64
+    python GN-MI-FGSM.py --comet_proj RQ1 --seed $seed --target $target --data_path $test_path --helpers_path $helpers_path --gpu cuda --model resnet50 --batch_size 64
 
     cd LGV
-    python LGV-MI-FGSM.py --comet_proj RQ1 --seed $seed --target $target --data_path $test_path --train_path $train_path --gpu cuda --model wide_resnet101_2 --batch_size 64 --lgv_models "/raid/data/mdjilani/lgv_models"
+    python LGV-MI-FGSM.py --comet_proj RQ1 --seed $seed --target $target --data_path $test_path --helpers_path $helpers_path --gpu cuda --model resnet50 --batch_size 64 --lgv_models "/raid/data/mdjilani/seed0"
 
     cd ..
   done
