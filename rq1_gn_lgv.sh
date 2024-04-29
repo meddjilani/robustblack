72d4b83b532a105b05e841f4121e3c456497113c
meddjilani <djilanimedben@gmail.com>
2024-04-30 01:51:54 +0200

    change order attack

--- rq1_gn_lgv.sh
+++ rq1_gn_lgv.sh
@@ -6,12 +6,12 @@ helpers_path="/home/mdjilani/robustblack/utils_robustblack"
 
 for seed in 42 1 10; do
   for target in "${my_list[@]}"; do
+    
+    cd LGV
+    python LGV-MI-FGSM.py --eps 0.0156862745 --comet_proj RQ1 --seed $seed --target $target --data_path $test_path --helpers_path $helpers_path --gpu cuda --model resnet50 --batch_size 64 --lgv_models "/raid/data/mdjilani/seed0"
 
     cd GHOST
     python GN-MI-FGSM.py --eps 0.0156862745 --comet_proj RQ1 --seed $seed --target $target --data_path $test_path --helpers_path $helpers_path --gpu cuda --model resnet50 --batch_size 64
-
-    cd LGV
-    python LGV-MI-FGSM.py --eps 0.0156862745 --comet_proj RQ1 --seed $seed --target $target --data_path $test_path --helpers_path $helpers_path --gpu cuda --model resnet50 --batch_size 64 --lgv_models "/raid/data/mdjilani/seed0"
 
     cd ..
   done
--- rq2_gn_lgv.sh
+++ rq2_gn_lgv.sh
@@ -7,11 +7,11 @@ helpers_path="/home/mdjilani/robustblack/utils_robustblack"
 for seed in 42 1 10; do
   for target in "${my_list[@]}"; do
 
-    cd GHOST
-    python GN-MI-FGSM.py --eps 0.0156862745 --comet_proj RQ2 --seed $seed --target $target --data_path $test_path --helpers_path $helpers_path --gpu cuda --model resnet50 --batch_size 64
-
     cd LGV
     python LGV-MI-FGSM.py --eps 0.0156862745 --comet_proj RQ2 --seed $seed --target $target --data_path $test_path --helpers_path $helpers_path --gpu cuda --model resnet50 --batch_size 64 --lgv_models "/raid/data/mdjilani/seed0"
+
+    cd GHOST
+    python GN-MI-FGSM.py --eps 0.0156862745 --comet_proj RQ2 --seed $seed --target $target --data_path $test_path --helpers_path $helpers_path --gpu cuda --model resnet50 --batch_size 64
 
     cd ..
   done
