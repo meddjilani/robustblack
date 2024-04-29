1dd043457b4d9544aebe569824fdf54d6fd0adc9
meddjilani <djilanimedben@gmail.com>
2024-04-30 00:53:45 +0200

    fix add prefix only for vanilla

--- LGV/LGV-MI-FGSM.py
+++ LGV/LGV-MI-FGSM.py
@@ -103,7 +103,10 @@ if __name__ == '__main__':
     loaded_models = []
     for i, filename in enumerate(os.listdir(args.lgv_models)):
         if filename[-3:] == ".pt":
-            source_model.load_state_dict(add_prefix(torch.load(os.path.join(args.lgv_models, filename), map_location=device)["state_dict"]))
+            if args.robust:
+                source_model.load_state_dict(torch.load(os.path.join(args.lgv_models, filename), map_location=device)["state_dict"])
+            else:
+                source_model.load_state_dict(add_prefix(torch.load(os.path.join(args.lgv_models, filename), map_location=device)["state_dict"]))
             source_model.eval()
             loaded_models.append(source_model)
             # if i+1 == num_lgv_models:
