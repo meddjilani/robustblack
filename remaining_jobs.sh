2383fec6b8a13923ec8d56285f941d29cdd702e3
meddjilani <djilanimedben@gmail.com>
2024-04-30 11:02:56 +0200

    script for remaining jobs

---
+++ remaining_jobs.sh
@@ -0,0 +1,11 @@
+my_list=( "Liu2023Comprehensive_ConvNeXt-L" "Bai2024MixedNUTS" "Liu2023Comprehensive_Swin-L" )
+
+test_path="/raid/data/mdjilani/dataset/val"
+
+for seed in 42 1 10; do
+  for target in "${my_list[@]}"; do
+
+    python ADMIX.py --eps 0.0156862745 --seed $seed --target $target --data_path $test_path --gpu cuda --model resnet50 --batch_size 64
+
+  done
+done
