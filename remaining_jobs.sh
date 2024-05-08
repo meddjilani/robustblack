4e8e29330eb5ebcce7380ede26b86826f1d5bbe5
meddjilani <djilanimedben@gmail.com>
2024-05-08 12:01:13 +0200

    tremba remaining jobs

--- remaining_jobs.sh
+++ remaining_jobs.sh
@@ -1,4 +1,4 @@
-my_list=( "Liu2023Comprehensive_ConvNeXt-L" "Bai2024MixedNUTS" "Liu2023Comprehensive_Swin-L" )
+my_list=( "Bai2024MixedNUTS" "Liu2023Comprehensive_Swin-L" )
 
 test_path="/raid/data/mdjilani/dataset/val"
 helpers_path="/home/mdjilani/robustblack/utils_robustblack"
@@ -8,6 +8,17 @@ for seed in 42; do
 
     cd TREMBA
     python attack.py --comet_proj RQ1 --seed $seed --model_name $target --device cuda --config config/attack_untarget.json --data_path $test_path --helpers_path $helpers_path --generator_name Imagenet_VGG16_Resnet18_Squeezenet_Googlenet_untarget --save_path /raid/data/mdjilani/tremba_save_path_vanilla
+
+    cd ..
+  done
+done
+
+
+for seed in 42; do
+  for target in "${my_list[@]}"; do
+
+    cd TREMBA
+    python attack.py --comet_proj RQ3 --seed $seed --model_name $target --device cuda --config config/attack_untarget.json --data_path $test_path --helpers_path $helpers_path --generator_name Imagenet_Wong2020Fast_Engstrom2019Robustness_Debenedetti2022Light_XCiT-M12_untarget --save_path /raid/data/mdjilani/tremba_save_path_robust
 
     cd ..
   done
