20a70ef687efae3ab2b04e76b258b8d122f8eb99
meddjilani <djilanimedben@gmail.com>
2024-05-21 14:32:00 +0200

    only for one seed

--- rq2_ifgsms_admix_uap.sh
+++ rq2_ifgsms_admix_uap.sh
@@ -2,7 +2,7 @@ my_list=( "Wong2020Fast" "Engstrom2019Robustness" )
 
 test_path="/raid/data/mdjilani/dataset/val"
 
-for seed in 42 1 10; do
+for seed in 42; do
   for target in "${my_list[@]}"; do
 
     python MI-FGSM.py --eps 0.0156862745 --seed $seed --target $target --data_path $test_path --gpu cuda --model resnet50 --batch_size 64
