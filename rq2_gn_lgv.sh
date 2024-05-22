7277c1d04d14aed4c58575b018574e043fa92f59
meddjilani <djilanimedben@gmail.com>
2024-05-22 08:33:50 +0200

    rq2 wong and eng targets

--- rq2_gn_lgv.sh
+++ rq2_gn_lgv.sh
@@ -1,4 +1,4 @@
-my_list=( "Liu2023Comprehensive_ConvNeXt-L" "Bai2024MixedNUTS" "Liu2023Comprehensive_Swin-L" )
+my_list=( "Wong2020Fast" "Engstrom2019Robustness" )
 
 test_path="/raid/data/mdjilani/dataset/val"
 helpers_path="/home/mdjilani/robustblack/utils_robustblack"
