f4ecf8c23edc7fa94c17198a43756f552053dd3a
meddjilani <djilanimedben@gmail.com>
2024-05-28 01:38:14 +0200

    reduce batch size to 16 for admix wong and eng

--- remaining_jobs_admix.sh
+++ remaining_jobs_admix.sh
@@ -5,7 +5,7 @@ test_path="/raid/data/mdjilani/dataset/val"
 for seed in 42; do
   for target in "${my_list[@]}"; do
 
-    python ADMIX.py --eps 0.0156862745 --seed $seed --target $target --data_path $test_path --gpu cuda --model resnet50 --batch_size 32
+    python ADMIX.py --eps 0.0156862745 --seed $seed --target $target --data_path $test_path --gpu cuda --model resnet50 --batch_size 16
 
   done
 done
