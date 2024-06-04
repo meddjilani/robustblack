c42e5084709692b388b093cd158feb780275cee0
meddjilani <djilanimedben@gmail.com>
2024-06-04 15:21:01 +0200

    remaining exps for admix against wong and engs

--- remaining_jobs3.sh
+++ remaining_jobs3.sh
@@ -1,16 +1,11 @@
-my_list=( "Liu2023Comprehensive_ConvNeXt-B" )
+my_list=( "Wong2020Fast" "Engstrom2019Robustness" )
 
 test_path="/raid/data/mdjilani/dataset/val"
-helpers_path="/home/mdjilani/robustblack/utils_robustblack"
-
-exp_root="/raid/data/mdjilani/bases_exp"
-adv_root="/raid/data/mdjilani/bases_adv"
 
 for seed in 42; do
   for target in "${my_list[@]}"; do
 
-    cd BASES
-    python query_w_bb.py --eps 4 --n_wb 10  --helpers_path $helpers_path --exp_root $exp_root --adv_root $adv_root --iterw 20 --seed $seed --victim $target --data_path $test_path --gpu cuda --comet_proj RQ1 -untargeted
+    python ADMIX.py --eps 0.0156862745 --seed $seed --target $target --data_path $test_path --gpu cuda --model resnet50 --batch_size 8
 
     cd ..
   done
