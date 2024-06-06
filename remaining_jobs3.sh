3e93fc845df4dee665de9993022f598cef8675de
meddjilani <djilanimedben@gmail.com>
2024-06-06 15:05:18 +0200

    fix tremba to bases exps

--- remaining_jobs3.sh
+++ remaining_jobs3.sh
@@ -3,11 +3,14 @@ my_list=( "Liu2023Comprehensive_ConvNeXt-L" )
 test_path="/raid/data/mdjilani/dataset/val"
 helpers_path="/home/mdjilani/robustblack/utils_robustblack"
 
+exp_root="/raid/data/mdjilani/bases_exp_rob"
+adv_root="/raid/data/mdjilani/bases_adv_rob"
+
 for seed in 42; do
   for target in "${my_list[@]}"; do
 
-    cd TREMBA
-    python attack.py --comet_proj RQ3 --seed $seed --model_name $target --device cuda --config config/attack_untarget.json --data_path $test_path --helpers_path $helpers_path --generator_name Imagenet_Wong2020Fast_Engstrom2019Robustness_Debenedetti2022Light_XCiT-M12_untarget --save_path /raid/data/mdjilani/tremba_save_path_robust
+    cd BASES
+    python query_w_bb.py --eps 4 --models Engstrom2019Robustness Wong2020Fast  --helpers_path $helpers_path --exp_root $exp_root --adv_root $adv_root --iterw 20 --seed $seed --victim $target --data_path $test_path --gpu cuda --comet_proj RQ3 -robust -untargeted
 
     cd ..
   done
