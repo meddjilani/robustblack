201c8ff990503e18bc7967c77fa6e1c42eb3fe05
meddjilani <djilanimedben@gmail.com>
2024-06-07 15:24:44 +0200

    comment lgv, waiting for collection of models

--- remaining_jobs.sh
+++ remaining_jobs.sh
@@ -7,7 +7,7 @@ for seed in 42; do
   for target in "${my_list[@]}"; do
 
     cd LGV
-    python LGV-MI-FGSM.py --eps 0.0156862745 --seed $seed --target $target --data_path $test_path --helpers_path $helpers_path --gpu cuda --model Liu2023Comprehensive_Swin-B --batch_size 64 --lgv_models "/raid/data/mdjilani/lgv_models_robust" --comet_proj RQ3 -robust
+#    python LGV-MI-FGSM.py --eps 0.0156862745 --seed $seed --target $target --data_path $test_path --helpers_path $helpers_path --gpu cuda --model Liu2023Comprehensive_Swin-B --batch_size 64 --lgv_models "/raid/data/mdjilani/lgv_models_robust" --comet_proj RQ3 -robust
 
     cd ../GHOST
     python GN-MI-FGSM.py --eps 0.0156862745 --seed $seed --target $target --data_path $test_path --gpu cuda --model Liu2023Comprehensive_Swin-B --batch_size 64 --comet_proj RQ3 -robust
