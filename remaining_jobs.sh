d9af17a0cf8a27aaa8ad62799e9fad63b6e586d2
meddjilani <djilanimedben@gmail.com>
2024-06-07 15:13:25 +0200

    lgv and ghost jobs for RQ3 using top 5 robust model as source

--- remaining_jobs.sh
+++ remaining_jobs.sh
@@ -1,16 +1,16 @@
-my_list=( "Liu2023Comprehensive_Swin-L" )
+my_list=( "Liu2023Comprehensive_ConvNeXt-L" "Bai2024MixedNUTS" "Liu2023Comprehensive_Swin-L" )
 
 test_path="/raid/data/mdjilani/dataset/val"
 helpers_path="/home/mdjilani/robustblack/utils_robustblack"
 
-exp_root="/raid/data/mdjilani/bases_exp_rob"
-adv_root="/raid/data/mdjilani/bases_adv_rob"
-
 for seed in 42; do
   for target in "${my_list[@]}"; do
 
-    cd BASES
-    python query_w_bb.py --eps 4 --models Engstrom2019Robustness Wong2020Fast  --helpers_path $helpers_path --exp_root $exp_root --adv_root $adv_root --iterw 20 --seed $seed --victim $target --data_path $test_path --gpu cuda --comet_proj RQ3 -robust -untargeted
+    cd LGV
+    python LGV-MI-FGSM.py --eps 0.0156862745 --seed $seed --target $target --data_path $test_path --helpers_path $helpers_path --gpu cuda --model Liu2023Comprehensive_Swin-B --batch_size 64 --lgv_models "/raid/data/mdjilani/lgv_models_robust" --comet_proj RQ3 -robust
+
+    cd ../GHOST
+    python GN-MI-FGSM.py --eps 0.0156862745 --seed $seed --target $target --data_path $test_path --gpu cuda --model Liu2023Comprehensive_Swin-B --batch_size 64 --comet_proj RQ3 -robust
 
     cd ..
   done
