227ac04573a450b75eadb5ce0a2c33ffdb8cf777
meddjilani <djilanimedben@gmail.com>
2024-06-07 18:26:48 +0200

    uncomment ghost jobs

--- remaining_jobs.sh
+++ remaining_jobs.sh
@@ -6,11 +6,11 @@ helpers_path="/home/mdjilani/robustblack/utils_robustblack"
 for seed in 42; do
   for target in "${my_list[@]}"; do
 
-    cd LGV
-    python LGV-MI-FGSM.py --eps 0.0156862745 --seed $seed --target $target --data_path $test_path --helpers_path $helpers_path --gpu cuda --model Liu2023Comprehensive_Swin-B --batch_size 64 --lgv_models "/raid/data/mdjilani/lgv_models_robust" --comet_proj RQ3 -robust
+#    cd LGV
+#    python LGV-MI-FGSM.py --eps 0.0156862745 --seed $seed --target $target --data_path $test_path --helpers_path $helpers_path --gpu cuda --model Liu2023Comprehensive_Swin-B --batch_size 64 --lgv_models "/raid/data/mdjilani/lgv_models_robust" --comet_proj RQ3 -robust
 
-#    cd ../GHOST
-#    python GN-MI-FGSM.py --eps 0.0156862745 --seed $seed --target $target --data_path $test_path --gpu cuda --model Liu2023Comprehensive_Swin-B --batch_size 64 --comet_proj RQ3 -robust
+    cd ../GHOST
+    python GN-MI-FGSM.py --eps 0.0156862745 --seed $seed --target $target --data_path $test_path --gpu cuda --model Liu2023Comprehensive_Swin-B --batch_size 16 --comet_proj RQ3 -robust
 
     cd ..
   done
