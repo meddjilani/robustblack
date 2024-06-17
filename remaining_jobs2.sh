7e76eae40ed7c44608553cf8f75bddf15c323ac7
meddjilani <djilanimedben@gmail.com>
2024-06-17 09:14:37 +0200

    jobs to validate MI suc rate

--- remaining_jobs2.sh
+++ remaining_jobs2.sh
@@ -1,17 +1,12 @@
-my_list=( "Bai2024MixedNUTS" )
+my_list=( "Wong2020Fast" "Engstrom2019Robustness" )
 
 test_path="/raid/data/mdjilani/dataset/val"
-helpers_path="/home/mdjilani/robustblack/utils_robustblack"
-
-exp_root="/raid/data/mdjilani/bases_exp_rob"
-adv_root="/raid/data/mdjilani/bases_adv_rob"
 
 for seed in 42; do
   for target in "${my_list[@]}"; do
 
-    cd BASES
-    python query_w_bb.py --eps 4 --models Engstrom2019Robustness Wong2020Fast  --helpers_path $helpers_path --exp_root $exp_root --adv_root $adv_root --iterw 20 --seed $seed --victim $target --data_path $test_path --gpu cuda --comet_proj RQ3 -robust -untargeted
+    python MI-FGSM.py --eps 0.0156862745 --seed $seed --target $target --data_path $test_path --gpu cuda --model resnet50 --batch_size 64
 
-    cd ..
+
   done
 done
