3e4252e10dbf3db00e964a8003759d5d09c0460b
meddjilani <djilanimedben@gmail.com>
2024-06-18 12:53:30 +0200

    correct path for ghost script

--- remaining_jobs4.sh
+++ remaining_jobs4.sh
@@ -6,7 +6,7 @@ helpers_path="/home/mdjilani/robustblack/utils_robustblack"
 for seed in 42; do
   for target in "${my_list[@]}"; do
 
-    cd ../GHOST
+    cd GHOST
     python GN-MI-FGSM.py --eps 0.0156862745 --seed $seed --target $target --data_path $test_path --gpu cuda --model Liu2023Comprehensive_Swin-B --batch_size 64 --comet_proj RQ3 -robust
 
     cd ..
