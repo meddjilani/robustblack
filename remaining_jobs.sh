682772a638c73ebfd1aea0afab23187a797f0e85
meddjilani <djilanimedben@gmail.com>
2024-06-24 12:20:04 +0200

    tremba rq3 top source 5 7

--- remaining_jobs.sh
+++ remaining_jobs.sh
@@ -1,16 +1,13 @@
-my_list=( "Liu2023Comprehensive_Swin-L" )
+my_list=( "Bai2024MixedNUTS" )
 
 test_path="/raid/data/mdjilani/dataset/val"
 helpers_path="/home/mdjilani/robustblack/utils_robustblack"
 
-exp_root="/raid/data/mdjilani/bases_exp_rob"
-adv_root="/raid/data/mdjilani/bases_adv_rob"
-
 for seed in 42; do
   for target in "${my_list[@]}"; do
 
-    cd BASES
-    python query_w_bb.py --eps 4 --models Liu2023Comprehensive_Swin-B Liu2023Comprehensive_ConvNeXt-B --helpers_path $helpers_path --exp_root $exp_root --adv_root $adv_root --iterw 20 --seed $seed --victim $target --data_path $test_path --gpu cuda --comet_proj RQ3 -robust -untargeted
+    cd TREMBA
+    python attack.py --comet_proj RQ3 --seed $seed --model_name $target --device cuda --config config/attack_untarget.json --data_path $test_path --helpers_path $helpers_path --generator_name 323Imagenet_Liu2023Comprehensive_Swin-B_Liu2023Comprehensive_ConvNeXt-B_untarget --save_path /raid/data/mdjilani/tremba_save_path_robust
 
     cd ..
   done
--- rq3_tremba.sh
+++ rq3_tremba.sh
@@ -7,7 +7,7 @@ for seed in 42; do
   for target in "${my_list[@]}"; do
 
     cd TREMBA
-    python attack.py --comet_proj RQ3 --seed $seed --model_name $target --device cuda --config config/attack_untarget.json --data_path $test_path --helpers_path $helpers_path --generator_name Imagenet_Wong2020Fast_Engstrom2019Robustness_Debenedetti2022Light_XCiT-M12_untarget --save_path /raid/data/mdjilani/tremba_save_path_robust
+    python attack.py --comet_proj RQ3 --seed $seed --model_name $target --device cuda --config config/attack_untarget.json --data_path $test_path --helpers_path $helpers_path --generator_name 323Imagenet_Liu2023Comprehensive_Swin-B_Liu2023Comprehensive_ConvNeXt-B_untarget --save_path /raid/data/mdjilani/tremba_save_path_robust
 
     cd ..
   done
