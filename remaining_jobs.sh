c294bc90200b45f486cb04ae58a189645b56cff1
meddjilani <djilanimedben@gmail.com>
2024-07-10 11:46:58 +0200

    add job lgv robust with robust peng

--- remaining_jobs.sh
+++ remaining_jobs.sh
@@ -1,13 +1,13 @@
-my_list=( "Liu2023Comprehensive_Swin-L" )
+my_list=( "Liu2023Comprehensive_ConvNeXt-L" "Bai2024MixedNUTS" "Liu2023Comprehensive_Swin-L" )
 
 test_path="/raid/data/mdjilani/dataset/val"
 helpers_path="/home/mdjilani/robustblack/utils_robustblack"
 
-for seed in 42; do
+for seed in 42 1 10; do
   for target in "${my_list[@]}"; do
 
-    cd TREMBA
-    python attack.py --comet_proj RQ3 --seed $seed --model_name $target --device cuda --config config/attack_untarget.json --data_path $test_path --helpers_path $helpers_path --generator_name Imagenet_Liu2023Comprehensive_Swin-B_Peng2023Robust_untarget --save_path /raid/data/mdjilani/tremba_save_path_robust
+    cd LGV
+    python LGV-MI-FGSM.py --eps 0.0156862745 --seed $seed --target $target --data_path $test_path --helpers_path $helpers_path --gpu cuda --model Peng2023Robust --batch_size 64 --lgv_models "/raid/data/mdjilani/lgv_robust_models_robustPeng2023Robust" --comet_proj RQ3 -robust
 
     cd ..
   done
