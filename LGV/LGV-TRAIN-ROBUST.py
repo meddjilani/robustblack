0f6d54124e7a693fc7795f575d6a7f1f90ce8794
meddjilani <djilanimedben@gmail.com>
2024-07-29 15:58:36 +0200

    add name of lgv train mode to the saved directory name

--- LGV/LGV-TRAIN-ROBUST.py
+++ LGV/LGV-TRAIN-ROBUST.py
@@ -59,7 +59,7 @@ if __name__ == '__main__':
                                                       })
     if args.robust:
         source_model = load_model(args.model, dataset = 'imagenet', threat_model = 'Linf').to(device)
-        path_save_models = args.save_models + 'lgv_robust_models_robust' + args.model
+        path_save_models = args.save_models + 'lgv_robust_models_robust' + args.model + "_" + args.train_mode
         
     else:
         source_model = load_model_torchvision(args.model, device, mean, std)
