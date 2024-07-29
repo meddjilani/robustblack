95d0561d87bf7930684f3a6d3029e71719a4d35d
meddjilani <djilanimedben@gmail.com>
2024-07-30 01:09:51 +0200

    lgv jobs for know distillation

--- remaining_jobs.sh
+++ remaining_jobs.sh
@@ -3,11 +3,12 @@ my_list=( "Liu2023Comprehensive_ConvNeXt-L" "Bai2024MixedNUTS" "Liu2023Comprehen
 test_path="/raid/data/mdjilani/dataset/val"
 helpers_path="/home/mdjilani/robustblack/utils_robustblack"
 
-for seed in 42 1 10; do
+for seed in 42; do
   for target in "${my_list[@]}"; do
 
     cd LGV
-    python LGV-MI-FGSM.py --eps 0.0156862745 --seed $seed --target $target --data_path $test_path --helpers_path $helpers_path --gpu cuda --model Peng2023Robust --batch_size 32 --lgv_models "/raid/data/mdjilani/lgv_robust_models_robustPeng2023Robust" --comet_proj RQ3 -robust
+    python LGV-MI-FGSM.py --eps 0.0156862745 --seed $seed --target $target --data_path $test_path --helpers_path $helpers_path --gpu cuda --model Peng2023Robust --batch_size 64 --lgv_models "/raid/data/mdjilani/lgv_robust_models_robustPeng2023Robust_know_dist" --comet_proj RQ3 -robust
+
 
     cd ..
   done
