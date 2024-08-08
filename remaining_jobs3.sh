c4bcc7ee77da77c3ff20e3874b4c8dc91ff7ac72
meddjilani <djilanimedben@gmail.com>
2024-08-08 19:02:17 +0200

    change file name

--- remaining_jobs3.sh
+++ remaining_jobs3.sh
@@ -7,7 +7,7 @@ for seed in 42; do
   for target in "${my_list[@]}"; do
 
     cd TREMBA
-    python attack.py --comet_proj RQ3 --seed $seed --model_name $target --device cuda --config config/attack_untarget.json --data_path $test_path --helpers_path $helpers_path --generator_name Imagenet_Liu2023Comprehensive_Swin-B_Peng2023Robust_untarget --save_path /raid/data/mdjilani/tremba_save_path_robust
+    python attack_ids.py --comet_proj RQ3 --seed $seed --model_name $target --device cuda --config config/attack_untarget.json --data_path $test_path --helpers_path $helpers_path --generator_name Imagenet_Liu2023Comprehensive_Swin-B_Peng2023Robust_untarget --save_path /raid/data/mdjilani/tremba_save_path_robust
 
     cd ..
   done
