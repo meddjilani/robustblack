9ad1bc475e7c930998f41f59ca76939947eff74d
meddjilani <djilanimedben@gmail.com>
2024-10-11 00:41:46 +0200

    42 seed missing for RQ2 v.2

--- rq2_ifgsms_admix_uap.sh
+++ rq2_ifgsms_admix_uap.sh
@@ -2,7 +2,7 @@ my_list=( "Liu2023Comprehensive_Swin-B" "Liu2023Comprehensive_ConvNeXt-B" "Singh
 
 test_path="/raid/data/mdjilani/dataset/val"
 
-for seed in 1 10 42; do
+for seed in 42; do
   for target in "${my_list[@]}"; do
 
     python MI-FGSM.py --eps 0.0156862745 --seed $seed --target $target --data_path $test_path --gpu cuda --model resnet50 --batch_size 64
