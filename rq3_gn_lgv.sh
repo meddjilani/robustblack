a40a034b929cc0ccd062b3cf9b6e5b381b620f01
meddjilani <djilanimedben@gmail.com>
2024-10-16 16:06:13 +0200

    only ghost rq3 v2

--- rq3_gn_lgv.sh
+++ rq3_gn_lgv.sh
@@ -1,4 +1,4 @@
-my_list=( "Salman2020Do_R18" "Standard_R50")
+my_list=("Liu2023Comprehensive_Swin-L" "Bai2024MixedNUTS" "Liu2023Comprehensive_ConvNeXt-L" "Salman2020Do_R18" "Standard_R50")
 
 
 test_path="/raid/data/mdjilani/dataset/val"
@@ -7,8 +7,8 @@ helpers_path="/home/mdjilani/robustblack/utils_robustblack"
 for seed in 1 10 42; do
   for target in "${my_list[@]}"; do
 
-    cd LGV
-    python LGV-MI-FGSM.py --eps 0.0156862745 --seed $seed --target $target --data_path $test_path --helpers_path $helpers_path --gpu cuda --model Engstrom2019Robustness --batch_size 64 --lgv_models "/raid/data/mdjilani/lgv_models_robust" --comet_proj RQ3 -robust
+#    cd LGV
+#    python LGV-MI-FGSM.py --eps 0.0156862745 --seed $seed --target $target --data_path $test_path --helpers_path $helpers_path --gpu cuda --model Engstrom2019Robustness --batch_size 64 --lgv_models "/raid/data/mdjilani/lgv_models_robust" --comet_proj RQ3 -robust
 
     cd ../GHOST
     python GN-MI-FGSM.py --eps 0.0156862745 --seed $seed --target $target --data_path $test_path --gpu cuda --model Peng2023Robust --batch_size 64 --comet_proj RQ3 -robust
