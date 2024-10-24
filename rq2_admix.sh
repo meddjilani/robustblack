fee915307021ee1f2e5c6c0de35db0d2eadac64e
meddjilani <djilanimedben@gmail.com>
2024-10-24 13:35:06 +0200

    admix wideresnet exps

--- rq2_admix.sh
+++ rq2_admix.sh
@@ -1,11 +1,11 @@
-my_list=( "Liu2023Comprehensive_Swin-B" "Liu2023Comprehensive_ConvNeXt-B" "Singh2023Revisiting_ViT-S-ConvStem" "Salman2020Do_50_2" "Salman2020Do_R50")
+my_list=("Liu2023Comprehensive_Swin-L" "Bai2024MixedNUTS" "Liu2023Comprehensive_ConvNeXt-L", "Liu2023Comprehensive_Swin-B" "Liu2023Comprehensive_ConvNeXt-B" "Singh2023Revisiting_ViT-S-ConvStem" "Salman2020Do_50_2" "Salman2020Do_R50", "Salman2020Do_R18", "Standard_R50")
 
 test_path="/raid/data/mdjilani/dataset/val"
 
-for seed in 42; do
+for seed in 1 10 42; do
   for target in "${my_list[@]}"; do
 
-    python ADMIX.py --eps 0.0156862745 --seed $seed --target $target --data_path $test_path --gpu cuda --model resnet50 --batch_size 16
+    python ADMIX.py --eps 0.0156862745 --seed $seed --target $target --data_path $test_path --gpu cuda --model wide_resnet50_2 --batch_size 16
 
   done
 done
