a0e9f5173e85fd91c06b87e48c5340e3f0ec61ea
meddjilani <djilanimedben@gmail.com>
2024-10-27 18:40:42 +0100

    lgv in wideresnet

--- rq2_gn_lgv.sh
+++ rq2_gn_lgv.sh
@@ -7,14 +7,14 @@ helpers_path="/home/mdjilani/robustblack/utils_robustblack"
 for seed in 1 10 42; do
   for target in "${my_list[@]}"; do
 
-#    cd LGV
-#    python LGV-MI-FGSM.py --eps 0.0156862745 --comet_proj RQ2 --seed $seed --target $target --data_path $test_path --helpers_path $helpers_path --gpu cuda --model wide_resnet50_2 --batch_size 64 --lgv_models "/raid/data/mdjilani/seed0"
-#
-#    cd ..
-
-    cd GHOST
-    python GN-MI-FGSM.py --eps 0.0156862745 --comet_proj RQ2 --seed $seed --target $target --data_path $test_path --helpers_path $helpers_path --gpu cuda --model wide_resnet50_2 --batch_size 64
+    cd LGV
+    python LGV-MI-FGSM.py --eps 0.0156862745 --comet_proj RQ2 --seed $seed --target $target --data_path $test_path --helpers_path $helpers_path --gpu cuda --model wide_resnet50_2 --batch_size 64 --lgv_models "/raid/data/mdjilani/${seed}lgv_modelswide_resnet50_2"
 
     cd ..
+
+#    cd GHOST
+#    python GN-MI-FGSM.py --eps 0.0156862745 --comet_proj RQ2 --seed $seed --target $target --data_path $test_path --helpers_path $helpers_path --gpu cuda --model wide_resnet50_2 --batch_size 64
+#
+#    cd ..
   done
 done
