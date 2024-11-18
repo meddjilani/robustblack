dbc58dec03f99902c139f7d12162420226016f1b
meddjilani <djilanimedben@gmail.com>
2024-11-18 19:31:19 +0100

    extending rq3 with 12 attacks

--- remaining_jobs2.sh
+++ remaining_jobs2.sh
@@ -1,11 +1,25 @@
-my_list=("Liu2023Comprehensive_ConvNeXt-L")
+my_list=( "Standard_R50" "Salman2020Do_R18" "Liu2023Comprehensive_ConvNeXt-L" "Bai2024MixedNUTS" "Liu2023Comprehensive_Swin-L" )
 
 test_path="/raid/data/mdjilani/dataset/val"
 
-for seed in 1 10 42; do
+for seed in 1; do
   for target in "${my_list[@]}"; do
 
-    python ADMIX.py --eps 0.0156862745 --seed $seed --target $target --data_path $test_path --gpu cuda --model wide_resnet50_2 --batch_size 16
+    python MI-FGSM.py --eps 0.0156862745 --seed $seed --target $target --data_path $test_path --gpu cuda --model wide_resnet101_2 --batch_size 64 -robust
 
+    python SGM-MI-FGSM.py --eps 0.0156862745 --seed $seed --target $target --data_path $test_path --gpu cuda --model wide_resnet101_2 --batch_size 64 -robust
+
+    python DI-FGSM.py --eps 0.0156862745 --seed $seed --target $target --data_path $test_path --gpu cuda --model wide_resnet101_2 --batch_size 64 -robust
+
+    python TI-FGSM.py --eps 0.0156862745 --seed $seed --target $target --data_path $test_path --gpu cuda --model wide_resnet101_2 --batch_size 64 -robust
+
+    python VMI-FGSM.py --eps 0.0156862745 --seed $seed --target $target --data_path $test_path --gpu cuda --model wide_resnet101_2 --batch_size 64 -robust
+
+    python VNI-FGSM.py --eps 0.0156862745 --seed $seed --target $target --data_path $test_path --gpu cuda --model wide_resnet101_2 --batch_size 64 -robust
+
+    cd pytorch-gd-uap
+    python train.py --eps 0.0156862745 --seed $seed --target $target --data_path $test_path --gpu cuda --model wide_resnet101_2 --batch_size 64 -robust
+
+    cd ..
   done
 done
