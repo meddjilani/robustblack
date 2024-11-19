9d783bea539b4996d17b28a282c715aaf34855b2
meddjilani <djilanimedben@gmail.com>
2024-11-19 12:13:08 +0100

    admix rq3 robsut

--- rq2_admix.sh
+++ rq2_admix.sh
@@ -1,11 +1,11 @@
-my_list=("Liu2023Comprehensive_ConvNeXt-L")
+my_list=( "Standard_R50" "Salman2020Do_R18" "Liu2023Comprehensive_ConvNeXt-L" "Bai2024MixedNUTS" "Liu2023Comprehensive_Swin-L" )
 
 test_path="/raid/data/mdjilani/dataset/val"
 
-for seed in 1 10 42; do
+for seed in 1; do
   for target in "${my_list[@]}"; do
 
-    python ADMIX.py --eps 0.0156862745 --seed $seed --target $target --data_path $test_path --gpu cuda --model wide_resnet50_2 --batch_size 16
+    python ADMIX.py --eps 0.0156862745 --seed $seed --target $target --data_path $test_path --gpu cuda --model Peng2023Robust --batch_size 8 -robust
 
   done
 done
