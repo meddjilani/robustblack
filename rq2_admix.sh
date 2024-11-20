0a312900a14fa126b61390811066bd54e2c05278
meddjilani <djilanimedben@gmail.com>
2024-11-20 01:16:30 +0100

    reduce further the batch size because of cuda out of memory

--- rq2_admix.sh
+++ rq2_admix.sh
@@ -1,11 +1,11 @@
-my_list=( "Standard_R50" "Salman2020Do_R18" "Liu2023Comprehensive_ConvNeXt-L" "Bai2024MixedNUTS" "Liu2023Comprehensive_Swin-L" )
+my_list=( "Standard_R50" "Liu2023Comprehensive_ConvNeXt-L" "Bai2024MixedNUTS" "Liu2023Comprehensive_Swin-L" )
 
 test_path="/raid/data/mdjilani/dataset/val"
 
 for seed in 1; do
   for target in "${my_list[@]}"; do
 
-    python ADMIX.py --eps 0.0156862745 --seed $seed --target $target --data_path $test_path --gpu cuda --model Peng2023Robust --batch_size 4 -robust
+    python ADMIX.py --eps 0.0156862745 --seed $seed --target $target --data_path $test_path --gpu cuda --model Peng2023Robust --batch_size 2 -robust
 
   done
 done
