d248a674d297b384b1d18806d624742435740857
meddjilani <djilanimedben@gmail.com>
2024-11-21 14:27:02 +0100

    ifgsm rq3 WRN sed 10

--- rq2_ifgsms_admix_uap.sh
+++ rq2_ifgsms_admix_uap.sh
@@ -2,7 +2,7 @@ my_list=( "Standard_R50" "Salman2020Do_R18" "Liu2023Comprehensive_ConvNeXt-L" "B
 
 test_path="/raid/data/mdjilani/dataset/val"
 
-for seed in 1 42; do
+for seed in 10; do
   for target in "${my_list[@]}"; do
 
     python MI-FGSM.py --eps 0.0156862745 --seed $seed --target $target --data_path $test_path --gpu cuda --model wide_resnet101_2 --batch_size 64
