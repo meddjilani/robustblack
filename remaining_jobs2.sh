fbd704c5446a3dae7babf1694abc6e62d32e8217
meddjilani <djilanimedben@gmail.com>
2024-12-16 00:30:27 +0100

    LGV with 3 seeds in RQ3 robust surrogate

--- remaining_jobs2.sh
+++ remaining_jobs2.sh
@@ -1,11 +1,15 @@
-my_list=( "Standard_R50" "Salman2020Do_R18" "Liu2023Comprehensive_ConvNeXt-L" "Bai2024MixedNUTS" "Liu2023Comprehensive_Swin-L" )
+my_list=("Standard_R50" "Liu2023Comprehensive_ConvNeXt-L" "Salman2020Do_R18" "Bai2024MixedNUTS" "Liu2023Comprehensive_Swin-L")
 
 test_path="/raid/data/mdjilani/dataset/val"
+helpers_path="/home/mdjilani/robustblack/utils_robustblack"
 
-for seed in 1 10; do
+
+for seed in 1; do
   for target in "${my_list[@]}"; do
 
-    python ADMIX.py --eps 0.0156862745 --seed $seed --target $target --data_path $test_path --gpu cuda --model wide_resnet101_2 --batch_size 4
+    cd LGV
+    python LGV-MI-FGSM.py --eps 0.0156862745 --comet_proj RQ2 --seed $seed --target $target --data_path $test_path --helpers_path $helpers_path --gpu cuda --model Peng2023Robust --batch_size 64 --lgv_models "/raid/data/mdjilani/${seed}lgv_models_robustPeng2023Robust" -robust
 
+    cd ..
   done
 done
--- remaining_jobs4.sh
+++ remaining_jobs4.sh
@@ -1,24 +1,14 @@
-my_list=( "Standard_R50" "Salman2020Do_R18" "Liu2023Comprehensive_ConvNeXt-L" "Bai2024MixedNUTS" "Liu2023Comprehensive_Swin-L" )
+my_list=("Standard_R50" "Liu2023Comprehensive_ConvNeXt-L" "Salman2020Do_R18" "Bai2024MixedNUTS" "Liu2023Comprehensive_Swin-L")
 
 test_path="/raid/data/mdjilani/dataset/val"
+helpers_path="/home/mdjilani/robustblack/utils_robustblack"
+
 
 for seed in 42; do
   for target in "${my_list[@]}"; do
 
-    python MI-FGSM.py --eps 0.0156862745 --seed $seed --target $target --data_path $test_path --gpu cuda --model Peng2023Robust --batch_size 32 -robust
-
-    python SGM-MI-FGSM.py --eps 0.0156862745 --seed $seed --target $target --data_path $test_path --gpu cuda --model Peng2023Robust --batch_size 32 -robust
-
-    python DI-FGSM.py --eps 0.0156862745 --seed $seed --target $target --data_path $test_path --gpu cuda --model Peng2023Robust --batch_size 32 -robust
-
-    python TI-FGSM.py --eps 0.0156862745 --seed $seed --target $target --data_path $test_path --gpu cuda --model Peng2023Robust --batch_size 32 -robust
-
-    python VMI-FGSM.py --eps 0.0156862745 --seed $seed --target $target --data_path $test_path --gpu cuda --model Peng2023Robust --batch_size 32 -robust
-
-    python VNI-FGSM.py --eps 0.0156862745 --seed $seed --target $target --data_path $test_path --gpu cuda --model Peng2023Robust --batch_size 32 -robust
-
-    cd pytorch-gd-uap
-    python train.py --eps 0.0156862745 --seed $seed --target $target --data_path $test_path --gpu cuda --model Peng2023Robust --batch_size 32 -robust
+    cd LGV
+    python LGV-MI-FGSM.py --eps 0.0156862745 --comet_proj RQ2 --seed $seed --target $target --data_path $test_path --helpers_path $helpers_path --gpu cuda --model Peng2023Robust --batch_size 64 --lgv_models "/raid/data/mdjilani/${seed}lgv_models_robustPeng2023Robust" -robust
 
     cd ..
   done
