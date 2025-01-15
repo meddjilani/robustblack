711a639b75597cdd80f530552153527431c9409c
meddjilani <djilanimedben@gmail.com>
2025-01-15 02:01:21 +0100

    attack new lgv except for robust model

--- remaining_jobs4.sh
+++ remaining_jobs4.sh
@@ -1,4 +1,4 @@
-my_list=("Standard_R50" "Liu2023Comprehensive_ConvNeXt-L" "Salman2020Do_R18" "Bai2024MixedNUTS" "Liu2023Comprehensive_Swin-L")
+my_list=("Standard_R50" "Salman2020Do_R50")
 
 test_path="/raid/data/mdjilani/dataset/val"
 helpers_path="/home/mdjilani/robustblack/utils_robustblack"
@@ -8,7 +8,7 @@ for seed in 42; do
   for target in "${my_list[@]}"; do
 
     cd LGV
-    python LGV-MI-FGSM.py --eps 0.0156862745 --comet_proj RQ2 --seed $seed --target $target --data_path $test_path --helpers_path $helpers_path --gpu cuda --model Peng2023Robust --batch_size 32 --lgv_models "/raid/data/mdjilani/${seed}lgv_models_robustPeng2023Robust" -robust
+    python LGV-MI-FGSM.py --eps 0.0156862745 --comet_proj RQ2 --seed $seed --target $target --data_path $test_path --helpers_path $helpers_path --gpu cuda --model Peng2023Robust --batch_size 32 --lgv_models "/raid/data/mdjilani/_FullTrain_${seed}_lgv_models_wide_resnet50_256"
 
     cd ..
   done
