346f0a59805a766a4baff26934d5ead108e7b6d9
meddjilani <djilanimedben@gmail.com>
2025-01-29 11:28:54 +0100

    split the seeds of RQ2 into two jobs because of the big number of target model

---
+++ remaining_jobs6.sh
@@ -0,0 +1,17 @@
+my_list=("Liu2023Comprehensive_Swin-L" "Bai2024MixedNUTS" "Liu2023Comprehensive_ConvNeXt-L" "Liu2023Comprehensive_Swin-B"  "Liu2023Comprehensive_ConvNeXt-B" "Singh2023Revisiting_ViT-S-ConvStem" "Salman2020Do_50_2" "Salman2020Do_R50" "Salman2020Do_R18")
+
+
+
+test_path="/home/mdjilani/datasets/val"
+helpers_path="/home/mdjilani/robustblack/utils_robustblack"
+
+
+for seed in 10 42; do
+  for target in "${my_list[@]}"; do
+
+    cd LGV
+    python LGV-MI-FGSM.py --eps 0.0156862745 --comet_proj RQ2 --seed $seed --target $target --data_path $test_path --helpers_path $helpers_path --gpu cuda --model resnet50 --batch_size 32 --lgv_models "/home/mdjilani/datasets/lgv_models/_FullTrain_${seed}_lgv_models_resnet50_256"
+
+    cd ..
+  done
+done
