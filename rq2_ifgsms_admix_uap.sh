a09c37a28f76d6688a029f7d8495b275a79282c5
meddjilani <djilanimedben@gmail.com>
2025-03-23 23:12:56 +0100

    data path for meteor

--- rq2_ifgsms_admix_uap.sh
+++ rq2_ifgsms_admix_uap.sh
@@ -1,6 +1,6 @@
 my_list=( "Salman2020Do_R18" "Salman2020Do_R50" "Salman2020Do_50_2" "Singh2023Revisiting_ViT-S-ConvStem" "Liu2023Comprehensive_ConvNeXt-B" "Liu2023Comprehensive_Swin-B" "Liu2023Comprehensive_ConvNeXt-L" "Bai2024MixedNUTS" "Liu2023Comprehensive_Swin-L" )
 
-test_path="/raid/data/mdjilani/dataset/val"
+test_path="/home/mdjilani/datasets/val"
 
 for seed in 42; do
   for target in "${my_list[@]}"; do
