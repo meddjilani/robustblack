0169abe0e83f2d97dc286f31022ccf1c05fd3c51
meddjilani <djilanimedben@gmail.com>
2025-03-24 15:54:53 +0100

    prepare rq2 fast for eps 16

--- rq1_gn_lgv.sh
+++ rq1_gn_lgv.sh
@@ -1,6 +1,6 @@
 my_list=( "Salman2020Do_R18" "Salman2020Do_R50" "Salman2020Do_50_2" "Singh2023Revisiting_ViT-S-ConvStem" "Liu2023Comprehensive_ConvNeXt-B" "Liu2023Comprehensive_Swin-B" "Liu2023Comprehensive_ConvNeXt-L" "Bai2024MixedNUTS" "Liu2023Comprehensive_Swin-L" )
 
-test_path="/raid/data/mdjilani/dataset/val"
+test_path="/home/mdjilani/datasets/val"
 helpers_path="/home/mdjilani/robustblack/utils_robustblack"
 
 
@@ -8,12 +8,12 @@ for seed in 42; do
   for target in "${my_list[@]}"; do
 
     cd LGV
-    python LGV-MI-FGSM.py --eps 0.03125 --comet_proj RQ1 --seed $seed --target $target --data_path $test_path --helpers_path $helpers_path --gpu cuda --model resnet50 --batch_size 64 --lgv_models "/raid/data/mdjilani/_FullTrain_42_lgv_models_resnet50_256"
+    python LGV-MI-FGSM.py --eps 0.062745 --comet_proj RQ1 --seed $seed --target $target --data_path $test_path --helpers_path $helpers_path --gpu cuda --model resnet50 --batch_size 64 --lgv_models "/home/mdjilani/datasets/lgv_models/_FullTrain_42_lgv_models_resnet50_256"
 
     cd ..
 
     cd GHOST
-    python GN-MI-FGSM.py --eps 0.03125 --comet_proj RQ1 --seed $seed --target $target --data_path $test_path --helpers_path $helpers_path --gpu cuda --model resnet50 --batch_size 64
+    python GN-MI-FGSM.py --eps 0.062745 --comet_proj RQ1 --seed $seed --target $target --data_path $test_path --helpers_path $helpers_path --gpu cuda --model resnet50 --batch_size 64
 
     cd ..
   done
--- rq2_ifgsms_admix_uap.sh
+++ rq2_ifgsms_admix_uap.sh
@@ -5,20 +5,20 @@ test_path="/home/mdjilani/datasets/val"
 for seed in 42; do
   for target in "${my_list[@]}"; do
 
-    python MI-FGSM.py --eps 0.03125 --seed $seed --target $target --data_path $test_path --gpu cuda --model resnet50 --batch_size 64
+    python MI-FGSM.py --eps 0.062745 --seed $seed --target $target --data_path $test_path --gpu cuda --model resnet50 --batch_size 64
 
-    python SGM-MI-FGSM.py --eps 0.03125 --seed $seed --target $target --data_path $test_path --gpu cuda --model resnet50 --batch_size 64
+    python SGM-MI-FGSM.py --eps 0.062745 --seed $seed --target $target --data_path $test_path --gpu cuda --model resnet50 --batch_size 64
 
-    python DI-FGSM.py --eps 0.03125 --seed $seed --target $target --data_path $test_path --gpu cuda --model resnet50 --batch_size 64
+    python DI-FGSM.py --eps 0.062745 --seed $seed --target $target --data_path $test_path --gpu cuda --model resnet50 --batch_size 64
 
-    python TI-FGSM.py --eps 0.03125 --seed $seed --target $target --data_path $test_path --gpu cuda --model resnet50 --batch_size 64
+    python TI-FGSM.py --eps 0.062745 --seed $seed --target $target --data_path $test_path --gpu cuda --model resnet50 --batch_size 64
 
-    python VMI-FGSM.py --eps 0.03125 --seed $seed --target $target --data_path $test_path --gpu cuda --model resnet50 --batch_size 64
+    python VMI-FGSM.py --eps 0.062745 --seed $seed --target $target --data_path $test_path --gpu cuda --model resnet50 --batch_size 64
 
-    python VNI-FGSM.py --eps 0.03125 --seed $seed --target $target --data_path $test_path --gpu cuda --model resnet50 --batch_size 64
+    python VNI-FGSM.py --eps 0.062745 --seed $seed --target $target --data_path $test_path --gpu cuda --model resnet50 --batch_size 64
 
     cd pytorch-gd-uap
-    python train.py --eps 0.03125 --seed $seed --target $target --data_path $test_path --gpu cuda --model resnet50 --batch_size 64
+    python train.py --eps 0.062745 --seed $seed --target $target --data_path $test_path --gpu cuda --model resnet50 --batch_size 64
 
     cd ..
   done
