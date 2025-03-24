4b77eb7e45b7516a2e3aa2c6b08f471df75bf74b
meddjilani <djilanimedben@gmail.com>
2025-03-24 04:20:49 +0100

    reduce batch size

--- remaining_jobs4.sh
+++ remaining_jobs4.sh
@@ -3,10 +3,11 @@ my_list=( "Standard_R50"  "Salman2020Do_R50" )
 test_path="/raid/data/mdjilani/dataset/val"
 
 
+
 for seed in 42; do
   for target in "${my_list[@]}"; do
 
-    python ADMIX.py --eps 0.03125 --seed $seed --target $target --data_path $test_path --gpu cuda --model wide_resnet50_2 --batch_size 16
+    python ADMIX.py --eps 0.03125 --seed $seed --target $target --data_path $test_path --gpu cuda --model wide_resnet50_2 --batch_size 8
 
   done
 done
