4938816a428505e308517a14c913ab724761c6a6
meddjilani <djilanimedben@gmail.com>
2025-03-25 03:27:42 +0100

    eps 16 for admix meteor

--- remaining_jobs4.sh
+++ remaining_jobs4.sh
@@ -1,13 +1,12 @@
 my_list=( "Standard_R50"  "Salman2020Do_R50" )
 
 test_path="/raid/data/mdjilani/dataset/val"
-
-
+test_path="/home/mdjilani/datasets/val"
 
 for seed in 42; do
   for target in "${my_list[@]}"; do
 
-    python ADMIX.py --eps 0.03125 --seed $seed --target $target --data_path $test_path --gpu cuda --model wide_resnet50_2 --batch_size 8
+    python ADMIX.py --eps 0.062745 --seed $seed --target $target --data_path $test_path --gpu cuda --model wide_resnet50_2 --batch_size 8
 
   done
 done
